52804618db3e4b434dc1b743e0c9d6e4f35ceebd
Mark Herman <mherman@iseatz.com>
2012-01-03 11:27:42 -0600

    Fixes Ruby 1.9 encoding on returned Strings.
    Ruby 1.9 associates an Encoding object with each string.  Strings were getting an ASCII Encoding before.  Protobuffers are only supposed to return UTF-8 strings.

--- lib/protocol_buffers/runtime/field.rb
+++ lib/protocol_buffers/runtime/field.rb
@@ -263,6 +263,18 @@ module ProtocolBuffers
       #
       # Maybe we just punt on this except in Ruby 1.9 where we can rely on the
       # language ensuring the string is always UTF-8?
+
+      def deserialize(value)
+        # To get bytes, the value was being read as ASCII.  Ruby 1.9 stores an encoding
+        # with its strings, and they were getting returned with Encoding ASCII-8BIT.
+        # Protobuffers are supposed to only return UTF-8 strings.  This attempts to
+        # force the encoding to UTF-8 if on Ruby 1.9 (force_encoding is defined on String).
+        read_value = value.read.to_s
+        if read_value.respond_to?("force_encoding")
+          read_value.force_encoding("UTF-8")
+        end
+        read_value
+      end
     end
 
     class NumericField < Field
